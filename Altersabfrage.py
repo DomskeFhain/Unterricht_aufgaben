1f6c285caf5a730d700bd20e4718e0c05b2a5092
Dominik Schelske (Domske) <190346505+DomskeFhain@users.noreply.github.com>
2024-12-17 15:52:16 +0100

    Update Altersabfrage.py

--- Altersabfrage.py
+++ Altersabfrage.py
@@ -1,10 +1,21 @@
+# 1
+
 def altersabfrage():
 
-    age = int(input("Wie alt bist du: "))
+    age_input = int(input("Wie alt bist du: "))
 
-    print(f"Du bist {age} Jahre alt")
+    print(f"Du bist {age_input} Jahre alt")
 
-altersabfrage()
-altersabfrage()
-altersabfrage()
+# 1.1
 
+def namensabfrage():
+    name = input("Wie ist dein Name: ")
+    print(f"Dein Name lautet: {name}")
+
+# 1.2
+
+def namensabfrage2():
+    name_input = input("Wie ist dein Name: ")
+    print(f"Dein Name lautet: {name_input}")
+    return name_input
+# wird nur angezeigt mit 'print(namensabfrage2):'
